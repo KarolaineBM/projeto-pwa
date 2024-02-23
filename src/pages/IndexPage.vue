c0c1b0f408c3636f8e6aa6784d8fb563442d1cba
KarolaineBM <karolainebarrosm@gmail.com>
2024-02-23 10:55:29 -0300

    ad pwa mode

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center bg-yellow-3">
+    <q-btn rounded color="red" class="botao">Gerente</q-btn>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+
+<style scoped>
+.botao {
+  font-size: 50px;
+  transition: 1s ease-in-out;
+}
+.botao:hover {
+  font-size: 100px;
+  box-shadow: 4px 4px 0px 0px rgba(0, 0, 0, 0.1);
+}
+</style>
